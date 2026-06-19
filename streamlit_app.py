d0d415e57f2981c4ae644ad7e7f67b34dafae8b5
varni03 <134824513+varni03@users.noreply.github.com>
2026-06-19 14:13:23 -0400

    Uncomment requests and data display for smoothie API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
 
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write(
@@ -38,6 +37,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
